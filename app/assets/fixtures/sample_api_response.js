83544b8cbb2dcb83c0d12f7d9fdb5794e1747a8f
Rutul Dave <rutul98@yahoo.com>
2017-01-12 15:52:26 -0500

    Assets for front-end exercise

---
+++ app/assets/fixtures/sample_api_response.js
@@ -0,0 +1,420 @@
+search_results({
+  "listings_count": 288971,
+  "listings": [{
+    "listing": {
+      "id": 715564,
+      "city": "London",
+      "picture_url": "https://a0.muscache.com/pictures/9665122/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/9665122/small.jpg",
+      "medium_url": "https://a1.muscache.com/pictures/9665122/medium.jpg",
+      "xl_picture_url": "https://a1.muscache.com/pictures/9665122/x_large.jpg",
+      "price": 89,
+      "native_currency": "USD",
+      "price_native": 92,
+      "price_formatted": "$92",
+      "lat": 51.48992726993278,
+      "lng": -0.13926175395032983,
+      "country": "United Kingdom",
+      "name": "Brand new studio in Victoria (8)",
+      "smart_location": "London, United Kingdom",
+      "instant_bookable": false,
+      "user_id": 580475,
+      "person_capacity": 2,
+      "neighborhood": "Pimlico",
+      "zipcode": "SW1V 2DB",
+      "state": "",
+      "beds": 1,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 580475,
+          "first_name": "Alessandro",
+          "picture_url": "https://a1.muscache.com/users/580475/profile_pic/1348416071/square_225.jpg"
+        }
+      },
+      "reviews_count": 19,
+      "address": "Gloucester St, London, SW1V 2DB, United Kingdom",
+      "country_code": "GB",
+      "cancellation_policy": "strict",
+      "room_type": "Entire home/apt",
+      "picture_urls": ["https://a0.muscache.com/pictures/9665122/large.jpg", "https://a2.muscache.com/pictures/9665173/large.jpg", "https://a0.muscache.com/pictures/9665250/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/9665122/small.jpg", "https://a1.muscache.com/pictures/9665173/small.jpg", "https://a0.muscache.com/pictures/9665250/small.jpg"],
+      "xl_picture_urls": ["https://a1.muscache.com/pictures/9665122/x_large.jpg", "https://a2.muscache.com/pictures/9665173/x_large.jpg", "https://a2.muscache.com/pictures/9665250/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 468225,
+      "city": "Paris",
+      "picture_url": "https://a0.muscache.com/pictures/5627758/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/5627758/small.jpg",
+      "medium_url": "https://a0.muscache.com/pictures/5627758/medium.jpg",
+      "xl_picture_url": "https://a2.muscache.com/pictures/5627758/x_large.jpg",
+      "price": 59,
+      "native_currency": "USD",
+      "price_native": 61,
+      "price_formatted": "$61",
+      "lat": 48.860520527348044,
+      "lng": 2.3618410095987366,
+      "country": "France",
+      "name": "Chambre independante dans le Marais",
+      "smart_location": "Paris, France",
+      "instant_bookable": true,
+      "user_id": 2324139,
+      "person_capacity": 2,
+      "neighborhood": "Le Marais",
+      "zipcode": "75003",
+      "state": "Île-de-France",
+      "beds": 1,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 2324139,
+          "first_name": "Léa",
+          "picture_url": "https://a0.muscache.com/users/2324139/profile_pic/1336509624/square_225.jpg"
+        }
+      },
+      "reviews_count": 59,
+      "address": "Rue de Poitou, Paris, Île-de-France 75003, France",
+      "country_code": "FR",
+      "cancellation_policy": "strict",
+      "room_type": "Private room",
+      "picture_urls": ["https://a0.muscache.com/pictures/5627758/large.jpg", "https://a0.muscache.com/pictures/5508619/large.jpg", "https://a0.muscache.com/pictures/5508296/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/5627758/small.jpg", "https://a2.muscache.com/pictures/5508619/small.jpg", "https://a0.muscache.com/pictures/5508296/small.jpg"],
+      "xl_picture_urls": ["https://a2.muscache.com/pictures/5627758/x_large.jpg", "https://a2.muscache.com/pictures/5508619/x_large.jpg", "https://a1.muscache.com/pictures/5508296/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 699704,
+      "city": "New Orleans",
+      "picture_url": "https://a1.muscache.com/pictures/9709661/large.jpg",
+      "thumbnail_url": "https://a0.muscache.com/pictures/9709661/small.jpg",
+      "medium_url": "https://a2.muscache.com/pictures/9709661/medium.jpg",
+      "xl_picture_url": "https://a0.muscache.com/pictures/9709661/x_large.jpg",
+      "price": 85,
+      "native_currency": "USD",
+      "price_native": 85,
+      "price_formatted": "$85",
+      "lat": 29.957580352317972,
+      "lng": -90.06807667340445,
+      "country": "United States",
+      "name": "Quiet French Quarter gem by Bourbon",
+      "smart_location": "New Orleans, LA",
+      "instant_bookable": false,
+      "user_id": 407251,
+      "person_capacity": 3,
+      "neighborhood": null,
+      "zipcode": "70112",
+      "state": "LA",
+      "beds": 2,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 407251,
+          "first_name": "Yarda",
+          "picture_url": "https://a2.muscache.com/users/407251/profile_pic/1340587363/square_225.jpg"
+        }
+      },
+      "reviews_count": 15,
+      "address": "Conti St, New Orleans, LA 70112, United States",
+      "country_code": "US",
+      "cancellation_policy": "moderate",
+      "room_type": "Entire home/apt",
+      "picture_urls": ["https://a1.muscache.com/pictures/9709661/large.jpg", "https://a2.muscache.com/pictures/9709693/large.jpg", "https://a0.muscache.com/pictures/9709701/large.jpg"],
+      "thumbnail_urls": ["https://a0.muscache.com/pictures/9709661/small.jpg", "https://a2.muscache.com/pictures/9709693/small.jpg", "https://a1.muscache.com/pictures/9709701/small.jpg"],
+      "xl_picture_urls": ["https://a0.muscache.com/pictures/9709661/x_large.jpg", "https://a1.muscache.com/pictures/9709693/x_large.jpg", "https://a1.muscache.com/pictures/9709701/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 605906,
+      "city": "Barcelona",
+      "picture_url": "https://a1.muscache.com/pictures/8442203/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/8442203/small.jpg",
+      "medium_url": "https://a1.muscache.com/pictures/8442203/medium.jpg",
+      "xl_picture_url": "https://a0.muscache.com/pictures/8442203/x_large.jpg",
+      "price": 34,
+      "native_currency": "USD",
+      "price_native": 35,
+      "price_formatted": "$35",
+      "lat": 41.385917944528444,
+      "lng": 2.1683054152448045,
+      "country": "Spain",
+      "name": "Nice apartment in the city center",
+      "smart_location": "Barcelona, Spain",
+      "instant_bookable": false,
+      "user_id": 3000844,
+      "person_capacity": 2,
+      "neighborhood": "El Raval",
+      "zipcode": "08001",
+      "state": "Cataluña",
+      "beds": 2,
+      "bedrooms": 2,
+      "user": {
+        "user": {
+          "id": 3000844,
+          "first_name": "Hugo",
+          "picture_url": "https://a0.muscache.com/users/3000844/profile_pic/1353093834/square_225.jpg"
+        }
+      },
+      "reviews_count": 32,
+      "address": "Carrer de Jovellanos, Barcelona, Cataluña 08001, Spain",
+      "country_code": "ES",
+      "cancellation_policy": "moderate",
+      "room_type": "Private room",
+      "picture_urls": ["https://a1.muscache.com/pictures/8442203/large.jpg", "https://a2.muscache.com/pictures/8442204/large.jpg", "https://a1.muscache.com/pictures/8442209/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/8442203/small.jpg", "https://a2.muscache.com/pictures/8442204/small.jpg", "https://a0.muscache.com/pictures/8442209/small.jpg"],
+      "xl_picture_urls": ["https://a0.muscache.com/pictures/8442203/x_large.jpg", "https://a1.muscache.com/pictures/8442204/x_large.jpg", "https://a1.muscache.com/pictures/8442209/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 629595,
+      "city": "Miami Beach",
+      "picture_url": "https://a0.muscache.com/pictures/8629561/large.jpg",
+      "thumbnail_url": "https://a0.muscache.com/pictures/8629561/small.jpg",
+      "medium_url": "https://a2.muscache.com/pictures/8629561/medium.jpg",
+      "xl_picture_url": "https://a0.muscache.com/pictures/8629561/x_large.jpg",
+      "price": 65,
+      "native_currency": "USD",
+      "price_native": 65,
+      "price_formatted": "$65",
+      "lat": 25.775484982366336,
+      "lng": -80.13555744410894,
+      "country": "United States",
+      "name": "Modern Cozy South Beach Studio",
+      "smart_location": "Miami Beach, FL",
+      "instant_bookable": true,
+      "user_id": 3126750,
+      "person_capacity": 3,
+      "neighborhood": "South Beach",
+      "zipcode": "33139",
+      "state": "FL",
+      "beds": 1,
+      "bedrooms": null,
+      "user": {
+        "user": {
+          "id": 3126750,
+          "first_name": "Vanessa",
+          "picture_url": "https://a1.muscache.com/users/3126750/profile_pic/1343849336/square_225.jpg"
+        }
+      },
+      "reviews_count": 20,
+      "address": "6th St, Miami Beach, FL 33139, United States",
+      "country_code": "US",
+      "cancellation_policy": "strict",
+      "room_type": "Entire home/apt",
+      "picture_urls": ["https://a0.muscache.com/pictures/8629561/large.jpg", "https://a0.muscache.com/pictures/8629292/large.jpg", "https://a1.muscache.com/pictures/8629770/large.jpg"],
+      "thumbnail_urls": ["https://a0.muscache.com/pictures/8629561/small.jpg", "https://a0.muscache.com/pictures/8629292/small.jpg", "https://a1.muscache.com/pictures/8629770/small.jpg"],
+      "xl_picture_urls": ["https://a0.muscache.com/pictures/8629561/x_large.jpg", "https://a0.muscache.com/pictures/8629292/x_large.jpg", "https://a0.muscache.com/pictures/8629770/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 604476,
+      "city": "Paris",
+      "picture_url": "https://a2.muscache.com/pictures/8486683/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/8486683/small.jpg",
+      "medium_url": "https://a0.muscache.com/pictures/8486683/medium.jpg",
+      "xl_picture_url": "https://a2.muscache.com/pictures/8486683/x_large.jpg",
+      "price": 118,
+      "native_currency": "USD",
+      "price_native": 122,
+      "price_formatted": "$122",
+      "lat": 48.860527997407054,
+      "lng": 2.3151293178339025,
+      "country": "France",
+      "name": "luxury flat Paris 7ème tour eiffel",
+      "smart_location": "Paris, France",
+      "instant_bookable": true,
+      "user_id": 2175694,
+      "person_capacity": 4,
+      "neighborhood": "Invalides - Ecole Militaire",
+      "zipcode": "75007",
+      "state": "Île-de-France",
+      "beds": 1,
+      "bedrooms": 2,
+      "user": {
+        "user": {
+          "id": 2175694,
+          "first_name": "Terikwa",
+          "picture_url": "https://a0.muscache.com/users/2175694/profile_pic/1343597954/square_225.jpg"
+        }
+      },
+      "reviews_count": 19,
+      "address": "Rue de l'Université, Paris, Île-de-France 75007, France",
+      "country_code": "FR",
+      "cancellation_policy": "flexible",
+      "room_type": "Entire home/apt",
+      "picture_urls": ["https://a2.muscache.com/pictures/8486683/large.jpg", "https://a0.muscache.com/pictures/8486698/large.jpg", "https://a2.muscache.com/pictures/8486721/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/8486683/small.jpg", "https://a2.muscache.com/pictures/8486698/small.jpg", "https://a2.muscache.com/pictures/8486721/small.jpg"],
+      "xl_picture_urls": ["https://a2.muscache.com/pictures/8486683/x_large.jpg", "https://a0.muscache.com/pictures/8486698/x_large.jpg", "https://a0.muscache.com/pictures/8486721/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 8357,
+      "city": "Aptos",
+      "picture_url": "https://a1.muscache.com/pictures/72251/large.jpg",
+      "thumbnail_url": "https://a0.muscache.com/pictures/72251/small.jpg",
+      "medium_url": "https://a0.muscache.com/pictures/72251/medium.jpg",
+      "xl_picture_url": "https://a2.muscache.com/pictures/72251/x_large.jpg",
+      "price": 100,
+      "native_currency": "USD",
+      "price_native": 100,
+      "price_formatted": "$100",
+      "lat": 37.007180809887615,
+      "lng": -121.88423857279582,
+      "country": "United States",
+      "name": "Mushroom Dome Cabin: #1 on airbnb",
+      "smart_location": "Aptos, CA",
+      "instant_bookable": false,
+      "user_id": 24281,
+      "person_capacity": 3,
+      "neighborhood": null,
+      "zipcode": "95003",
+      "state": "CA",
+      "beds": 2,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 24281,
+          "first_name": "Kitty & Michael",
+          "picture_url": "https://a0.muscache.com/users/24281/profile_pic/1291101200/square_225.jpg"
+        }
+      },
+      "reviews_count": 316,
+      "address": "Trout Gulch Rd, Aptos, CA 95003, United States",
+      "country_code": "US",
+      "cancellation_policy": "moderate",
+      "room_type": "Entire home/apt",
+      "picture_urls": ["https://a1.muscache.com/pictures/72251/large.jpg", "https://a2.muscache.com/pictures/136844/large.jpg", "https://a0.muscache.com/pictures/136845/large.jpg"],
+      "thumbnail_urls": ["https://a0.muscache.com/pictures/72251/small.jpg", "https://a1.muscache.com/pictures/136844/small.jpg", "https://a0.muscache.com/pictures/136845/small.jpg"],
+      "xl_picture_urls": ["https://a2.muscache.com/pictures/72251/x_large.jpg", "https://a2.muscache.com/pictures/136844/x_large.jpg", "https://a0.muscache.com/pictures/136845/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 574700,
+      "city": "London",
+      "picture_url": "https://a1.muscache.com/pictures/7560453/large.jpg",
+      "thumbnail_url": "https://a0.muscache.com/pictures/7560453/small.jpg",
+      "medium_url": "https://a1.muscache.com/pictures/7560453/medium.jpg",
+      "xl_picture_url": "https://a1.muscache.com/pictures/7560453/x_large.jpg",
+      "price": 89,
+      "native_currency": "USD",
+      "price_native": 92,
+      "price_formatted": "$92",
+      "lat": 51.53221955983912,
+      "lng": -0.11976375218944332,
+      "country": "United Kingdom",
+      "name": "Central, Airy & Comfortable Bedroom",
+      "smart_location": "London, United Kingdom",
+      "instant_bookable": false,
+      "user_id": 2094916,
+      "person_capacity": 2,
+      "neighborhood": "Islington",
+      "zipcode": "N1 9JR",
+      "state": "",
+      "beds": 1,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 2094916,
+          "first_name": "Emile",
+          "picture_url": "https://a2.muscache.com/users/2094916/profile_pic/1343119892/square_225.jpg"
+        }
+      },
+      "reviews_count": 30,
+      "address": "Pentonville Rd, London, N1 9JR, United Kingdom",
+      "country_code": "GB",
+      "cancellation_policy": "flexible",
+      "room_type": "Private room",
+      "picture_urls": ["https://a1.muscache.com/pictures/7560453/large.jpg", "https://a0.muscache.com/pictures/7562814/large.jpg", "https://a1.muscache.com/pictures/7560569/large.jpg"],
+      "thumbnail_urls": ["https://a0.muscache.com/pictures/7560453/small.jpg", "https://a2.muscache.com/pictures/7562814/small.jpg", "https://a0.muscache.com/pictures/7560569/small.jpg"],
+      "xl_picture_urls": ["https://a1.muscache.com/pictures/7560453/x_large.jpg", "https://a1.muscache.com/pictures/7562814/x_large.jpg", "https://a1.muscache.com/pictures/7560569/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 240938,
+      "city": "Paris",
+      "picture_url": "https://a1.muscache.com/pictures/2277998/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/2277998/small.jpg",
+      "medium_url": "https://a1.muscache.com/pictures/2277998/medium.jpg",
+      "xl_picture_url": "https://a1.muscache.com/pictures/2277998/x_large.jpg",
+      "price": 66,
+      "native_currency": "USD",
+      "price_native": 68,
+      "price_formatted": "$68",
+      "lat": 48.86979750644146,
+      "lng": 2.353684976057792,
+      "country": "France",
+      "name": "ROOM IN A CHARMING FLAT WITH TERRAC",
+      "smart_location": "Paris, France",
+      "instant_bookable": false,
+      "user_id": 1264200,
+      "person_capacity": 2,
+      "neighborhood": "Bourse (II Arrondissement)",
+      "zipcode": "75002",
+      "state": "Île-de-France",
+      "beds": 1,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 1264200,
+          "first_name": "Valérie",
+          "picture_url": "https://a2.muscache.com/users/1264200/profile_pic/1318270518/square_225.jpg"
+        }
+      },
+      "reviews_count": 42,
+      "address": "Rue Blondel, Paris, Île-de-France 75002, France",
+      "country_code": "FR",
+      "cancellation_policy": "flexible",
+      "room_type": "Private room",
+      "picture_urls": ["https://a1.muscache.com/pictures/2277998/large.jpg", "https://a1.muscache.com/pictures/2278003/large.jpg", "https://a1.muscache.com/pictures/2278006/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/2277998/small.jpg", "https://a1.muscache.com/pictures/2278003/small.jpg", "https://a1.muscache.com/pictures/2278006/small.jpg"],
+      "xl_picture_urls": ["https://a1.muscache.com/pictures/2277998/x_large.jpg", "https://a2.muscache.com/pictures/2278003/x_large.jpg", "https://a0.muscache.com/pictures/2278006/x_large.jpg"]
+    }
+  }, {
+    "listing": {
+      "id": 220558,
+      "city": "Half Moon Bay",
+      "picture_url": "https://a1.muscache.com/pictures/2155034/large.jpg",
+      "thumbnail_url": "https://a2.muscache.com/pictures/2155034/small.jpg",
+      "medium_url": "https://a0.muscache.com/pictures/2155034/medium.jpg",
+      "xl_picture_url": "https://a2.muscache.com/pictures/2155034/x_large.jpg",
+      "price": 84,
+      "native_currency": "USD",
+      "price_native": 84,
+      "price_formatted": "$84",
+      "lat": 37.50331585615467,
+      "lng": -122.48800708274645,
+      "country": "United States",
+      "name": "Half Moon Bay Seahouse Room 1",
+      "smart_location": "Half Moon Bay, CA",
+      "instant_bookable": false,
+      "user_id": 1097013,
+      "person_capacity": 2,
+      "neighborhood": null,
+      "zipcode": "94019",
+      "state": "CA",
+      "beds": 1,
+      "bedrooms": 1,
+      "user": {
+        "user": {
+          "id": 1097013,
+          "first_name": ".. H + M",
+          "picture_url": "https://a1.muscache.com/users/1097013/profile_pic/1315797671/square_225.jpg"
+        }
+      },
+      "reviews_count": 118,
+      "address": "Broadway, Half Moon Bay, CA, Half Moon Bay, CA 94019, United States",
+      "country_code": "US",
+      "cancellation_policy": "moderate",
+      "room_type": "Private room",
+      "picture_urls": ["https://a1.muscache.com/pictures/2155034/large.jpg", "https://a0.muscache.com/pictures/2155061/large.jpg", "https://a0.muscache.com/pictures/2155042/large.jpg"],
+      "thumbnail_urls": ["https://a2.muscache.com/pictures/2155034/small.jpg", "https://a2.muscache.com/pictures/2155061/small.jpg", "https://a1.muscache.com/pictures/2155042/small.jpg"],
+      "xl_picture_urls": ["https://a2.muscache.com/pictures/2155034/x_large.jpg", "https://a0.muscache.com/pictures/2155061/x_large.jpg", "https://a1.muscache.com/pictures/2155042/x_large.jpg"]
+    }
+  }],
+  "meta": {
+    "price_type": "nightly",
+    "price_range_min_native": 10,
+    "price_range_max_native": 300,
+    "native_currency": "USD"
+  }
+})
