0144699dcbec2db12e1add8b85f3aed062f4e31c
Tiffan Phan <thetiffanyphan@gmail.com>
2018-04-02 23:42:43 -0700

    rename dictionary, separate logic from string interpolation

--- exercise_solutions/exercise_01.js
+++ exercise_solutions/exercise_01.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const scoreDictionary = {
+const githubEventDictionary = {
   'IssuesEvent': 7,
   'IssueCommentEvent': 6,
   'PushEvent': 5,
@@ -11,16 +11,19 @@ const scoreDictionary = {
 
 const scoreGithubEvents = () => {
   axios.get('https://api.github.com/users/dhh/events/public')
+
     .then(response => {
+      const score = response.data.reduce((a, c) => {
+        return a + (githubEventDictionary[c.type] || 1);
+      }, 0);
       console.log(
-        `DHH\'s github score is ${response.data.reduce((a, c) => {
-          return a + (scoreDictionary[c.type] || 1);
-        }, 0)}`
+        `DHH\'s github score is ${score}`
       );
     })
+    
     .catch(err => {
       console.log(err);
-    })
+    });
 };
 
 module.exports = scoreGithubEvents();
